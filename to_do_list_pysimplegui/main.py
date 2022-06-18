8c3611de7f9c513af3a7a2398747139e0939e840
itsWesne <106451416+itsWesne@users.noreply.github.com>
2022-06-18 12:53:33 -0300

    To do List com PySimpleGui

---
+++ to_do_list_pysimplegui/main.py
@@ -0,0 +1,28 @@
+import PySimpleGUI as sg
+
+#   Criação do layout
+def window_layout():
+    sg.theme('Black')
+    row = [
+        [sg.Input(''), sg.Checkbox('')]
+    ]
+    layout = [
+        [sg.Frame('TASKS', layout=row, key='container')],
+        [sg.Button('New Task'), sg.Button('Reset')]
+    ]
+
+    return sg.Window('To do list', layout=layout, finalize=True)
+
+#   Criar janela
+window = window_layout()
+
+#   Regras da janela
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED:
+        break
+    elif event == 'New Task':
+        window.extend_layout(window['container'],[[sg.Input(''), sg.Checkbox('')]])
+    elif event == 'Reset':
+        window.close()
+        window = window_layout()
