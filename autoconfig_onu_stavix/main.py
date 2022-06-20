78ab5d0a05118cf475d6f0502be9f449514fb072
itsWesne <106451416+itsWesne@users.noreply.github.com>
2022-06-20 20:30:41 -0300

    Adicionado input de ip address, ajustes no layout da  wan_settings

--- autoconfig_onu_stavix/main.py
+++ autoconfig_onu_stavix/main.py
@@ -3,8 +3,11 @@ import PySimpleGUI as sg
 # Configurações do Layout
 sg.theme('DarkGrey11')
 
+ip_address = [
+    [sg.Text('IP:'), sg.Input(default_text='192.168.1.1', size=(33, 1))]
+]
 wan_settings = [
-    [sg.Text('TYPE:'), sg.Listbox(default_values=['PPPoe'], values=('PPPoe', 'Bridge'), size=(10, 2)), sg.Push()],
+    [sg.Text('TYPE:'), sg.Combo(default_value='PPPoe', button_arrow_color='DarkGrey', values=('PPPoe', 'Bridge'), size=(10, 2)), sg.Push()],
     [sg.Text('VLAN:'), sg.Input(default_text='2800', size=(12, 1)), sg.Push()],
     [sg.Text('PPPoe LOGIN:', size=(13, 1)), sg.Input(size=(15, 1)), sg.Push()],
     [sg.Text('PASSWORD:', size=(13, 1)), sg.Input(size=(15, 1), default_text='102030'), sg.Push()],
@@ -12,9 +15,9 @@ wan_settings = [
     [sg.Checkbox('LAN1', default=True), sg.Checkbox('LAN2', default=True), sg.Checkbox('LAN3', default=True), sg.Checkbox('LAN4', default=True)]
 ]
 wifi_settings = [
-    [sg.Text('WIFI 2.4G:', size=(10, 1)), sg.Input(default_text='_2.4G', size=(23, 1)), sg.Push()],
+    [sg.Text('WIFI 2.4G:', size=(10, 1)), sg.Input(size=(23, 1)), sg.Push()],
     [sg.Text('PASSWORD', size=(10, 1)), sg.Input(size=(23, 1)), sg.Push()],
-    [sg.Text('WIFI 5.8G:', size=(10, 1)), sg.Input(default_text='_5G', size=(23, 1)), sg.Push()],
+    [sg.Text('WIFI 5.8G:', size=(10, 1)), sg.Input(size=(23, 1)), sg.Push()],
     [sg.Text('PASSWORD', size=(10, 1)), sg.Input(size=(23, 1)), sg.Push()]
 ]
 admin_settings = [
@@ -31,6 +34,7 @@ others = [
     [sg.Checkbox('PING', default=True)]
 ]
 layout = [
+    [sg.Frame('IP ADDRESS', layout=ip_address)],
     [sg.Frame('WAN SETTINGS', layout=wan_settings)],
     [sg.Frame('WIFI SETTINGS', layout=wifi_settings)],
     [sg.Frame('ADMIN SETTINGS', layout=admin_settings)],
